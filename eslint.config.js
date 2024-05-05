77e4e07ae745e764105d8b74b5a54e00dc1ce6e6
brown <86018533+2brownc@users.noreply.github.com>
2024-05-05 15:18:35 +0530

    remove old config files

--- eslint.config.js
+++
@@ -1,9 +0,0 @@
-const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended")
-
-module.exports = {
-  rules: {
-    "no-console": "error",
-  },
-  // Any other config imports go at the top
-  ...eslintPluginPrettierRecommended,
-}
